32a94231ee31227ca52d0e6acd90043ce0ab0354
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-20 22:12:43 +0300

    added toggle password

--- static/js/login.js
+++ static/js/login.js
@@ -1,3 +1,11 @@
 const seePasswordSignup = () =>{
-    
+    let password = document.getElementById("passwords")
+    let cpassword = document.getElementById("cpasswords")
+    if(password.type == "password" || cpassword.type == "password"){
+        password.type = "text";
+        cpassword.type = "text";
+    }else{
+        password.type = "password";
+        cpassword.type = "password";
+    }
 }
