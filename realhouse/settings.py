122fc7fd301bc826b8f9cce69d456e031a2218b0
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-20 18:31:57 +0300

    added base file

--- realhouse/settings.py
+++ realhouse/settings.py
@@ -21,6 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+DEBUG = True
+
 SECRET_KEY = config('SECRET_KEY')
 
 ALLOWED_HOSTS = []
--- realhouse/urls.py
+++ realhouse/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from login.views import login_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', login_view , name="login"),
 ]
