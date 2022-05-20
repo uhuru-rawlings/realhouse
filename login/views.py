884dee676fbeb0a8120bc8f5ded718365168ef98
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-20 23:43:51 +0300

    added post request handler

--- login/views.py
+++ login/views.py
@@ -1,8 +1,14 @@
 from multiprocessing import context
 from django.shortcuts import render
+from django.contrib.auth.hashers import make_password,check_password
+
+
 
 # Create your views here.
 def login_view(request):
+    if request.method == 'POST':
+        useremail = request.POST['useremail']
+        password = request.POST['password']
     context = {
         'title':'RealEstate - Login'
     }
