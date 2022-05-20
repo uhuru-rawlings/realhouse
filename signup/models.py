0a71d03d915a901e1f8fe9493b9df4b4f6eb8dbf
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-21 00:27:50 +0300

    added registration model

--- signup/models.py
+++ signup/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Registration(models.Model):
+    useremail = models.EmailField(max_length=200)
+    password = models.CharField(max_length=300)
+    date_added = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(delfault= True)
+
+    class Meta:
+        db_table = 'Registration'
+
+    def __str__(self):
+        return self.useremail
