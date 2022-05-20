9eb2ad208fad7a1493381a0360604bdba0deb80d
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-20 22:51:32 +0300

    added validate signup

--- static/js/login.js
+++ static/js/login.js
@@ -19,6 +19,10 @@ const tooglePasswordLogin = () =>{
     }
 }
 
+const removeError = (clicked_id) =>{
+    document.getElementById(clicked_id).style.borderColor = "#CED4DA";
+}
+
 const validateLogin = (event) =>{
     event.preventDefault()
     let username = document.getElementById("useremail");
@@ -32,6 +36,47 @@ const validateLogin = (event) =>{
         }else{
             password.style.borderColor = 'red';
             return
+        }
+    }else{
+        if(rememberme.checked){
+            let userdet = {
+                'useremail':useremail.value,
+                'password':password.value
+            }
+
+            localStorage.setItem("userdet", JSON.stringify(userdet))
+        }
+    }
+}
+
+const setUser = () =>{
+    let username = document.getElementById("useremail");
+    let password = document.getElementById("password");
+    
+    let details = localStorage.getItem("userdet")
+    if(details){
+        let userobj = JSON.parse(details);
+        username.value = userobj.useremail;
+        password.value = userobj.password;
+    }
+}
+
+window.onload= setUser;
+
+
+const validateSignup = (event) =>{
+    event.preventDefault()
+    let useremails = document.getElementById('useremails')
+    let passwords = document.getElementById('passwords')
+    let cpasswords = document.getElementById('cpasswords')
+
+    if(useremails.value.trim() === '' || passwords.value.trim() === ''|| cpasswords.value.trim() === ''){
+        if(useremails.value.trim() === ''){
+            useremails.style.borderColor = "red";
+        }else if(passwords.value.trim() === ''){
+            passwords.style.borderColor = "red";
+        }else{
+            cpasswords.style.borderColor = "red";
         }
     }
 }
