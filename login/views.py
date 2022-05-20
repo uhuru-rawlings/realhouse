48828b0a19e5da0d4ac4c826de33081d5c80f7d2
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-20 17:39:06 +0300

    added bootstrap and fontawesome

--- login/views.py
+++ login/views.py
@@ -1,3 +1,9 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 # Create your views here.
+def login_view(request):
+    context = {
+        'title':'RealEstate - Login'
+    }
+    return render(request, "login.html",context)
