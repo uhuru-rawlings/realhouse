57d3df1b3b0f0bbe905b292cb3f8c9d996b6a8bb
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-21 20:16:00 +0300

    registered contact page

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def contact_view(request):
+    context= {
+        'title':'RealEstate - Contact'
+    }
+    return render(request, "contact.html", context)
