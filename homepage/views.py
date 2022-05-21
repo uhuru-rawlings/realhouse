0c7b3bbd38eac94c962fb193e5ef966a8e3632b0
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-21 08:32:28 +0300

    added homepage view

--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,9 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 # Create your views here.
+def homepage_view(request):
+    context = {
+        'title':'RealEstate - Home Page'
+    }
+    return render(request,"homepage.html",context)
