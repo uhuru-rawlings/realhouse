a9f33da021182054b335158b5a4882f1c189b224
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-23 10:26:29 +0300

    added houses model

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Houses(models.Model):
+    location = models.CharField(max_length=100)
+    bedrooms = models.IntegerField()
+    areaofland = models.IntegerField()
+    areaofhouse = models.IntegerField()
+    price = models.IntegerField()
+    interiorimage1 = models.ImageField(upload_to = 'images')
+    interiorimage2 = models.ImageField(upload_to = 'images')
+    exteriorimage = models.ImageField(upload_to = 'images')
+
+    class Meta:
+        db_table = 'Houses'
+
+    def __str__(self):
+        return self.location
