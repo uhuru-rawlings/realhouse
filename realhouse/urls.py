88861979b2c602212ed42edcd13854ca5a2a4169
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-23 11:06:40 +0300

    added details link

--- realhouse/urls.py
+++ realhouse/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from login.views import login_view
 from signup.views import signup_view
-from homepage.views import homepage_view,houses_view,logout_view,about_view
+from homepage.views import homepage_view,houses_view,logout_view,about_view,details_view
 from contact.views import contact_view
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,5 +27,6 @@ urlpatterns = [
     path('home/houses/', houses_view , name="houses"),
     path('home/logout/', logout_view , name="logout"),
     path('home/about/', about_view , name="about"),
+    path('home/details/<int:id>/', details_view , name="details"),
     path('houses/contact/', contact_view , name="contact"),
 ]
