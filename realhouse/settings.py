47e9bccd799e3c86c71a97d3c607a6d9002f348d
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-23 22:54:32 +0300

    added middle ware classes

--- realhouse/settings.py
+++ realhouse/settings.py
@@ -166,6 +166,11 @@ STATICFILES_DIRS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
